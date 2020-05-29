01ad4403fd9073fc89bbce8529e6dff535734ff7
Cassio Faleiro <cassio.faleiro@icloud.com>
2020-05-29 20:46:43 -0300

    Alter File Name

--- src/Projeto/TelaPrincipal.java
+++ src/Projeto/TelaPrincipal.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Atividade2 {
+public class TelaPrincipal {
 
     public static void main(String[] args) {
        menu();
